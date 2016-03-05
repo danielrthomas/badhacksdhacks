1e56dcc0e9cd1daf61b741becb43e89ebd464de9
Armaan Shah <armaanshah96@gmail.com>
2016-03-05 16:22:19 -0600

    adding armaan's js stuff, and changing html tags

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
 //asdfadf
+
+function output() {
+    for (var i=0; i<10; i++) {
+        var button = document.getElementById("button-" + i);
+        if(button != null) {
+            button.onclick = function(e) {
+            disp = disp + e.target.id.substring(7);
+            display.innerHTML = disp;
+            }
+        }
+    }
+};
