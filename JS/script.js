d6644bac8725edf9093ab4aa7e84b0c516f431fa
Daniel Thomas <dandudex@dhcp-10-105-94-65.wireless.northwestern.private>
2016-03-05 17:58:55 -0600

    nearly working js

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ var entry = "";
 var savedValue = "";
 var isValue = false;
 var operation = "";
+var eqPressed = false;
 
 var one = document.getElementById("button-1");
 var two = document.getElementById("button-2");
@@ -20,14 +21,23 @@ var minus = document.getElementById("button--");
 var multiply = document.getElementById("button-*");
 var divide = document.getElementById("button-/");
 
-function display(varia){
+var equals = document.getElementById("button-=")
+
+function displayer(varia){
 	display.innerHTML = varia;
 }
 
 function output(number) {
+	if(eqPressed){
+		savedValue = "";
+		isValue = false;
+		entry = "";
+		operation = "";
+		eqPressed = false;
+	}
 	entry = entry + number;
-	display(entry);
-	entry = entry.parseFloat();
+	displayer(entry);
+	entry = parseFloat(entry);
 }
 
 one.onclick = function(e){output('1');}
@@ -42,6 +52,10 @@ nine.onclick = function(e){output('9');}
 zero.onclick = function(e){output('0');}
 
 function operator(ope){
+	if(eqPressed){
+		savedValue = entry;
+		entry = "";
+	}
 	if(isValue){
 		operate();
 	}
@@ -51,3 +65,38 @@ function operator(ope){
 	entry = "";
 }
 
+// assuming operation != "", assuming entry and savedValue both have fields
+function operate(){
+	if(operation == "+"){
+		entry = (parseFloat(savedValue) + parseFloat(entry)).toString();
+	}
+	else if(operation == "-"){
+		entry = (parseFloat(savedValue) - parseFloat(entry)).toString();
+	}
+	else if(operation == "*"){
+		entry = (parseFloat(savedValue) * parseFloat(entry)).toString();
+	}
+	else if(operation == "/"){
+		entry = (parseFloat(savedValue) / parseFloat(entry)).toString();
+	}
+	savedValue = "";
+	isValue = false;
+	displayer(entry);
+}
+//ends with savedValue = "", isValue = false, operation = ""
+
+plus.onclick = function(e){operator('+');}
+minus.onclick = function(e){operator('-');}
+multiply.onclick = function(e){operator('*');}
+divide.onclick = function(e){operator('/');}
+
+equals.onclick = function(e){
+	if(operation != ""){
+		if(!isValue){
+			isValue = true;
+			savedValue = entry;
+		}
+		operate();
+		eqPressed = true;
+	}
+}
